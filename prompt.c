c5279d487be0ab5ac1cf87bbbf6fc635ff2e4ec5
JayTeeSF <jonathan@his-service.net>
2014-07-26 23:42:38 -0700

    1st working command-line interface: prompt.c & .h

--- prompt.c
+++ prompt.c
@@ -1,63 +1,73 @@
 #include "prompt.h"
+#include "jt_debug.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <editline/readline.h>
-/*
- * #include <editline/history.h>
- * static char input[2048]; // user input buffer
- **/
 
-static char version[] = "0.1a";
-static char minimal_prompt[] = "> ";
+const char version[] = "0.1a";
+const int exit_string_count = 2;
+char *exit_strings[exit_string_count] = {"exit", "quit"};
+const char minimal_prompt[] = "> ";
 
 void welcome_message(void) {
-  puts("Welcome to Jtlisp\n");
-  // FIXME: type 'exit' or 'quit'
-  puts("Press Ctrl+c to Exit\n");
+  puts("Welcome to Jtlisp");
+  puts("Type 'exit' or 'quit' (not including the single-quotes) to Exit");
 }
 
 void echo_message(char *message) {
-  printf("No you're a %s", message);
+  printf("No you're a %s\n", message);
 }
 
-/*
-   void echo_prompt(void) {
-   echo_message(prompt());
-   }
-   */
-
-void respond_to(char *parsed_input) { 
-  echo_message(parsed_input);
+int found(char *search_list[], char *key, int size) {
+  for (int n = 0; n < size; ++n) {
+    if (!strcmp(search_list[n],key)) {
+      debug("n is %d\n", n);
+      return 1;
+    }
+  }
+  return 0;
 }
 
-char *parse(char *input) { 
+int found_an_exit_in(char *input_token) {
+  if (found(exit_strings, input_token, exit_string_count) > 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// FIXME: allow for an many tokens
+void respond_to(char *parsed_input) {
+  if (found_an_exit_in(parsed_input)) {
+    debug("found the exit\n");
+    exit(0);
+  } else {
+    echo_message(parsed_input);
+  }
+}
+
+char *parse(char *input) {
   return input;
 }
 
-void allocate_prompt(char *prompt_string) {
+char *generate_prompt(void) {
   int prompt_size = strlen(version) + strlen(minimal_prompt);
+  char *prompt_string;
   prompt_string = malloc(prompt_size * sizeof(char*));
   bzero(prompt_string, prompt_size);
   strcat(prompt_string, version);
   strcat(prompt_string, "> ");
+  return prompt_string;
 }
 
-char *prompt(void) {
-  /*
-   * fputs("jtlisp> ", stdout);
-   * return fgets(input, sizeof(input), stdin);
-   **/
-  char *prompt_string = NULL;
-  allocate_prompt(prompt_string);
-
+void prompt(void) {
+  char *prompt_string = generate_prompt();
   char *input = readline(prompt_string);
   add_history(input);
-  respond_to(parse(input));
 
+  respond_to(parse(input));
   free(prompt_string);
   free(input);
-
-  return input;
 }
--- prompt.h
+++ prompt.h
@@ -1,9 +1,12 @@
 #ifndef _PROMPT_H_
 #define _PROMPT_H_
 void welcome_message(void);
-char *prompt(void);
-void allocate_prompt(char *prompt_string);
+void prompt(void);
+//void allocate_prompt(char *prompt_string);
+char *generate_prompt(void);
+int found(char *search_list[], char *key, int size);
 void echo_message(char *message);
+int found_an_exit_in(char *input_token);
 void respond_to(char *parsed_input);
 char *parse(char *input);
 #endif
